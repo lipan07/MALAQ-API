2784b6c6be9a9fa3138ae453d784f1d2d41e2967
lipan07 <lipan.biswas.dev@gmail.com>
2024-09-16 19:34:02 +0530

    added post others changes

--- app/Models/PostOther.php
+++ app/Models/PostOther.php
@@ -16,4 +16,18 @@ class PostOther extends Model
         'description',
         'amount',
     ];
+
+    public static function restructureStoreData($data)
+    {
+        $restructuredData = [
+            'post_id' => $data['post_id'] ?? null,
+            'amount' => $data['amount'] ?? null,
+            'title' => $data['adTitle'] ?? null,
+            'description' => $data['description'] ?? null,
+            // Add other fields you need to restructure or process
+        ];
+
+        // Save the restructured data
+        return self::create($restructuredData);
+    }
 }
