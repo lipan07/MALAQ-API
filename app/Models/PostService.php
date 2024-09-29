8a71965ee8931065f1a7e23528a96ca560359527
lipan07 <lipan.biswas.dev@gmail.com>
2024-09-29 13:58:18 +0530

    post service

--- app/Models/PostService.php
+++ app/Models/PostService.php
@@ -12,8 +12,24 @@ class PostService extends Model
 
     protected $fillable = [
         'post_id',
+        'type',
         'title',
         'description',
         'amount',
     ];
+
+    public static function restructureStoreData($data)
+    {
+        $restructuredData = [
+            'post_id' => $data['post_id'] ?? null,
+            'type' => $data['type'] ?? null,
+            'amount' => $data['amount'] ?? null,
+            'title' => $data['adTitle'] ?? null,
+            'description' => $data['description'] ?? null,
+            // Add other fields you need to restructure or process
+        ];
+
+        // Save the restructured data
+        return self::create($restructuredData);
+    }
 }
