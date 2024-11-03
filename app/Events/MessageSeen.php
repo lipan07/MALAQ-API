5ab4a8ad06bc05a271196ebf9858ac7aa1bdc984
lipan07 <lipan.biswas.dev@gmail.com>
2024-11-03 22:04:43 +0530

    message seen event added

--- app/Events/MessageSeen.php
+++ app/Events/MessageSeen.php
@@ -2,22 +2,17 @@
 
 namespace App\Events;
 
-use App\Models\Message;
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSeen
+class MessageSeen  implements ShouldBroadcast
 {
     use SerializesModels;
 
     public $message;
 
-    public function __construct(Message $message)
+    public function __construct($message)
     {
         $this->message = $message;
     }
