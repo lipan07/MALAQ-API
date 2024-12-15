82ccf11c7e38ef934be4717680d48604936d7e07
lipan07 <lipan.biswas.dev@gmail.com>
2024-12-16 00:55:11 +0530

    get following

--- app/Models/PostFollower.php
+++ app/Models/PostFollower.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class PostFollower extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
 
     protected $fillable = ['user_id', 'post_id',  'post_user_id'];
 
