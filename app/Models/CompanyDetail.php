b31d659dfe8d94921fc98f14b574de484559ee21
lipan07 <lipan.biswas.dev@gmail.com>
2025-05-11 02:00:59 +0530

    get profile api added

--- app/Models/CompanyDetail.php
+++ app/Models/CompanyDetail.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class CompanyDetail extends Model
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable, HasUuids;
 
     protected $table = 'company_details';
 
