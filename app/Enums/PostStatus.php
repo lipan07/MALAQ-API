a4a667e2700a159b08fd0900bf575c38f216bfd1
lipan07 <lipan.biswas.dev@gmail.com>
2025-06-19 23:55:21 +0530

    post status

--- app/Enums/PostStatus.php
+++ app/Enums/PostStatus.php
@@ -9,6 +9,9 @@ enum PostStatus: string
     case Active = 'active';
     case Inactive = 'inactive';
     case Failed = 'failed';
+    case Sold = 'sold';
+    case Blocked = 'blocked';
+    case Deleted = 'deleted';
 
     /**
      * Returns the default type.
@@ -27,6 +30,6 @@ enum PostStatus: string
      */
     public static function allStatus(): array
     {
-        return array_map(fn ($case) => $case->value, self::cases());
+        return array_map(fn($case) => $case->value, self::cases());
     }
 }
