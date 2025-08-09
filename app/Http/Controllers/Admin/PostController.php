3a525099cf080b706a0e92cdfae09d5cbef39985
lipan07 <lipan.biswas.dev@gmail.com>
2025-08-09 17:19:32 +0530

    post list and images

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\PostStatus;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
@@ -11,9 +12,18 @@ class PostController extends Controller
     public function index()
     {
         // Fetch posts with related data and paginate
-        $posts = Post::with(['user', 'category'])
-            ->orderBy('created_at', 'desc')
+        $posts = Post::orderBy('created_at', 'desc')
+            ->where('status', PostStatus::Pending)
             ->paginate(10);
+
+        $posts->load([
+            'user',
+            'category',
+            'images',
+        ]);
+
+        // $posts = Post::with(['user', 'category', 'images'])->orderBy('created_at', 'desc')->paginate(10);
+
 
         return view('admin.posts.index', compact('posts'));
     }
@@ -21,7 +31,7 @@ class PostController extends Controller
     public function approve($id)
     {
         $post = Post::findOrFail($id);
-        $post->status = 'approved'; // Assuming your PostStatus enum allows this
+        $post->status = PostStatus::Active; // Assuming your PostStatus enum allows this
         $post->save();
 
         return redirect()->back()->with('success', 'Post approved successfully.');
