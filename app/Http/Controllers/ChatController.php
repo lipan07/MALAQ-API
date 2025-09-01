78ec2f1dac66663b588b7e7931144821a502818d
lipan07 <lipan.biswas.dev@gmail.com>
2025-09-01 23:59:52 +0530

    chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -135,7 +135,7 @@ class ChatController extends Controller
         $messages = Message::select('id', 'user_id', 'message', 'created_at', 'updated_at', 'is_seen')
             ->where('chat_id', $chat->id)
             ->where('created_at', '>=', now()->subDays(3))
-            ->orderBy('created_at', 'desc')
+            ->orderBy('created_at', 'asc')
             ->get();
 
         return response()->json([
