e4f157cc7bdbaff3a88fb6a0874ef68fa6a66cc0
lipan07 <lipan.biswas.dev@gmail.com>
2025-10-08 11:34:06 +0530

    user id

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -142,6 +142,10 @@ class PostController extends Controller
                 $postsQuery->where('title', 'LIKE', '%' . $request->search . '%');
             }
 
+            if ($request->filled('user_id')) {
+                $postsQuery->where('user_id', $request->user_id);
+            }
+
             // Apply listing type filter if provided
             if ($request->filled('listingType')) {
                 $postsQuery->where('type', $request->listingType ?? PostType::defaultType()->value);
