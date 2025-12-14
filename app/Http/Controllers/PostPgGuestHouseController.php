d3021239924effd2cb4336460ade9495ce2107c1
lipan07 <lipan.biswas.dev@gmail.com>
2025-12-14 20:48:59 +0530

    update

--- app/Http/Controllers/PostPgGuestHouseController.php
+++ app/Http/Controllers/PostPgGuestHouseController.php
@@ -6,7 +6,7 @@ use App\Http\Requests\StorePgGuestHouseRequest;
 use App\Http\Requests\UpdatePgGuestHouseRequest;
 use App\Models\PostPgGuestHouse;
 
-class PostPostPgGuestHouseController extends Controller
+class PostPgGuestHouseController extends Controller
 {
     /**
      * Display a listing of the resource.
